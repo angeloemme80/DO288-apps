0973d047564234020d59f1073792aef28d76fe84
angelo <angelo@localhost.localdomain>
2020-11-17 21:24:27 +0100

    d

--- post-commit/angelo.sh
+++ post-commit/angelo.sh
@@ -1,6 +1,6 @@
 #curl -s -S -i -X POST http://ang-angelino.apps.eu45.prod.nextcle.com/api/builds -f -d 'developer=angelo&git=da_bach&project=prg'
 echo "script lanciato"
 date +"%d/%m/%Y"
-echo "<?php print \"aggiunto dopo \"; ?>" >> index.php
+echo "<?php print \"aggiunto dopo \"; ?>" >> ./index.php
 
 echo "script finito"
